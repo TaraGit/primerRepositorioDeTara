9222b800f41dffae12642fd8ca2f263d36819642
TaraGit <eirin_luna@hotmail.es>
2013-03-01 08:54:17 +0000

    Probando...

--- HolaGit/src/Git.java
+++ HolaGit/src/Git.java
@@ -3,7 +3,8 @@ public class Git {
 	
 	public static void main (String [] args) {
 		
-		System.out.println("Primer cambio");
+		System.out.println("Segundo cambio");
+		System.out.println("cambiamos cambiamos");
 		
 	}
 	
